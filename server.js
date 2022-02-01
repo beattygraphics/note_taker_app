742982186e27bb31a423f5c951fac796c9aa6c51
Erin Beatty <beattygraphics@icloud.com>
2022-01-31 22:30:39 -0500

    updated server.js file and attempted deployement to heroku

--- server.js
+++ server.js
@@ -1,19 +1,17 @@
-// DEPENDENCIES
 const express = require("express");
+const apiRoutes = require("./routes/apiRoutes");
+const htmlRoutes = require("./routes/htmlRoutes");
 
 const PORT = process.env.PORT || 3002;
 const app = express();
 
 // MIDDLEWARE
-app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-
-
+app.use(express.urlencoded({ extended: true }));
 app.use(express.static("public"));
+app.use("/api", apiRoutes);
+app.use("/", htmlRoutes);
 
-
-require("/routes/apiRoutes")(app);
-require("/routes/htmlRoutes")(app);
 
 app.listen(PORT, () =>
   console.log(`Express server currently running on port: ${PORT}`)
